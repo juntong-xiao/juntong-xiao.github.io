2cc9a2acd558ee0d6391bd7d9d2d0ed3d5825fab
juntong-xiao <74118173+juntong-xiao@users.noreply.github.com>
2021-01-15 22:54:25 -0500

    Create click.js

---
+++ js/click.js
@@ -0,0 +1,11 @@
+var count=0;
+var btn=document.getElementById('edu_btn');
+btn.addEventListener("click",function(){
+  count++;
+  if (count%2==0) {
+    btn.innerHTML="CLICK TO SHOW MORE";
+  }
+  else {
+    btn.innerHTML="CLICK TO SHOW LESS";
+  }
+});
